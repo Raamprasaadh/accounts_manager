8d7bc2b2539927c2c2fbe7029cd11f539b68d427
Raamprasaadh <ramprasadhsurya@gmail.com>
2021-01-13 11:23:07 +0530

    adds accounts schema

---
+++ server/models/accounts.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const accountsSchema = new Schema({
+    date:{
+        type:date,
+        required:true
+    },
+    accounts_type:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    sub_type:{
+        type:String,
+        required:true,
+        minlength:6
+    },
+    amount:{
+        type:Number,
+        required:true,
+    },
+    reason:{
+        type:String,
+        required:true,
+    }
+},{
+    timestamps:true
+});
+
+const Accounts = mongoose.model(accounts, accountsSchema);
+
+module.exports = Accounts;
